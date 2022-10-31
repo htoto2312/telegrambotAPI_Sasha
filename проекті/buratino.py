b6f00a71c292833410b9be2dd7b62d5122a00d00
htoto2312 <116834216+htoto2312@users.noreply.github.com>
2022-10-31 16:25:37 +0200

    Delete проекті directory

--- проекті/buratino.py
+++
@@ -1,6 +0,0 @@
-n,l,t,m=map(int,input().split())
-m=m//t
-for i in range(0,m,1):
-    n=n*l
-
-print(n)
--- проекті/flowers.py
+++
@@ -1,6 +0,0 @@
-v,r,p,b=map(int,input().split())
-x,y=v//b,r//p
-if y>=x:
-    print(x)
-elif x>y:
-    print(y)
